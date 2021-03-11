33a7a1a58c5e0739acd16c5631b49ebe93867e70
Linamikaela21 <mikaelagutierrezarribas@gmail.com>
2021-03-11 10:03:20 -0300

    arrays

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // No cambies los nombres de las funciones.
 
 function devolverPrimerElemento(array) {
@@ -84,24 +83,24 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-  var promedio = 0
-  for(let i = 0; i < resultadosTest.length; i++) {
-  	promedio = promedio + resultadosTest[i]
+  var promedio = 0 //Acumulador
+  for(let i = 0; i < resultadosTest.length; i++) { //recorro todo el array
+  	promedio = promedio + resultadosTest[i] // sumo todos los valores a la variable promedio [1, 2, 3]
 	}
-		return promedio / resultadosTest.length
+		return promedio / resultadosTest.length // y divido el promedio por la cantidad de datos -> 6/3
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-   var maximo = numeros[0];
-  for(var i = 1; i < numeros.length; i++) {
-    if(numeros[i] > maximo) { 
-      maximo = numeros[i];
+   var maximo = numeros[0]; //Defino un elemtento del array como un maximo temporal
+  for(var i = 1; i < numeros.length; i++) { //Recorro todo el array
+    if(numeros[i] > maximo) {  //Si algun numero del array es mayor a mi maximo temporal
+      maximo = numeros[i]; //Intercambio su valor para crear un nuevo MAXIMO
     }
   }
-  return maximo;
+  return maximo; //Devuelvo el numero maximo creado
 }
 
 function multiplicarArgumentos() {
@@ -109,10 +108,11 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
-if(arguments.length < 1) return 0;
-  var total = 1;
-  for(var i = 0; i < arguments.length; i++) {
-    total = total * arguments[i];
+
+if(arguments.length < 1) return 0; //Arguments es un array dado si es menor a uno significa que esta vacio
+  var total = 1; //Comienzo en 1 porque todo numero multiplicado por 0 es 0
+  for(var i = 0; i < arguments.length; i++) { //Recorro el array
+    total = total * arguments[i]; 
   }
   return total;
 }
@@ -134,140 +134,3 @@ module.exports = {
   numeroMasGrande,
   multiplicarArgumentos,
 };
-=======
-// No cambies los nombres de las funciones.
-
-function devolverPrimerElemento(array) {
-  // Devuelve el primer elemento de un array
-  // Tu código:
-  return array [0]
-}
-
-function devolverUltimoElemento(array) {
-  // Devuelve el último elemento de un array
-  // Tu código:
-  return array [array.length -1]
-}
-
-function obtenerLargoDelArray(array) {
-  // Devuelve el largo de un array
-  // Tu código:
-  return array.length 
-}
-
-function incrementarPorUno(array) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Aumenta cada entero por 1
-  // y devuelve el array
-  // Tu código:
-  for (let i = 0; i < array.length; i++) {
-  	array[i] = array[i] + 1
-  }
-  return array
-}
-
-function agregarItemAlFinalDelArray(array, elemento) {
-  // Añade el "elemento" al final del array
-  // y devuelve el array
-  // Tu código:
-  	array [array.length] = elemento
-  return array
-  }
-  
-
-function agregarItemAlComienzoDelArray(array, elemento) {
-  // Añade el "elemento" al comienzo del array
-  // y devuelve el array
-  // Pista: usa el método `.unshift`
-  // Tu código:
-  	array.unshift (elemento) 
-  	return array
-}
-
-function dePalabrasAFrase(palabras) {
-  // "palabras" es un array de strings/cadenas
-  // Devuelve un string donde todas las palabras estén concatenadas
-  // con espacios entre cada palabra
-  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
-  // Tu código:
-  return palabras.join (' ');
-}
-
-function arrayContiene(array, elemento) {
-  // Comprueba si el elemento existe dentro de "array"
-  // Devuelve "true" si está, o "false" si no está
-  // Tu código:
-
-for (let i = 0; i < array.length; i++) {
-		if(array[i] === elemento) {
-			return true
-		}
-	}	
-return false
-}
-
-function agregarNumeros(numeros) {
-  // "array" debe ser una matriz de enteros (int/integers)
-  // Suma todos los enteros y devuelve el valor
-  // Tu código:
-  var suma = 0
-  for(let i = 0; i < numeros.length; i++) {
-  	suma = suma + numeros[i]
-	}
-		return suma;
-	}
-function promedioResultadosTest(resultadosTest) {
-  // "resultadosTest" debe ser una matriz de enteros (int/integers)
-  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
-  // Tu código:
-  var promedio = 0
-  for(let i = 0; i < resultadosTest.length; i++) {
-  	promedio = promedio + resultadosTest[i]
-	}
-		return promedio / resultadosTest.length
-}
-
-function numeroMasGrande(numeros) {
-  // "numeros" debe ser una matriz de enteros (int/integers)
-  // Devuelve el número más grande
-  // Tu código:
-   var maximo = numeros[0];
-  for(var i = 1; i < numeros.length; i++) {
-    if(numeros[i] > maximo) { 
-      maximo = numeros[i];
-    }
-  }
-  return maximo;
-}
-
-function multiplicarArgumentos() {
-  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
-  // Si no se pasan argumentos devuelve 0
-  // Si se pasa un argumento, simplemente devuélvelo
-  // Tu código:
-if(arguments.length < 1) return 0;
-  var total = 1;
-  for(var i = 0; i < arguments.length; i++) {
-    total = total * arguments[i];
-  }
-  return total;
-}
-
-// No modificar nada debajo de esta línea
-// --------------------------------
-
-module.exports = {
-  devolverPrimerElemento,
-  devolverUltimoElemento,
-  obtenerLargoDelArray,
-  incrementarPorUno,
-  agregarItemAlFinalDelArray,
-  agregarItemAlComienzoDelArray,
-  dePalabrasAFrase,
-  arrayContiene,
-  agregarNumeros,
-  promedioResultadosTest,
-  numeroMasGrande,
-  multiplicarArgumentos,
-};
->>>>>>> c59a5940069cb629a43c48b8c0401e7b2b5623c1
